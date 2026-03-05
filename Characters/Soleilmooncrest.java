6226c38e5b82d38742ad302cfa426eec961102e1
ken <kennithan12@gmail.com>
2026-03-05 16:31:32 +0800

    Finished kenneth,zakkar,mooncrest characters

---
+++ Characters/Soleilmooncrest.java
@@ -0,0 +1,71 @@
+package Characters;
+
+import Foundation.GameCharacter;
+import Foundation.Skill;
+
+public class SoleilMooncrest extends GameCharacter {
+
+    private Skill moonStrike;
+    private Skill moonlightShine;
+    private Skill shadowBlast;
+
+    public SoleilMooncrest() {
+        super("Soleil Mooncrest", "Human", "Moonlit Witch", 200, 50, 100);
+
+        moonStrike = new Skill("Moon Strike", 10, 0, 10, 0);
+        moonlightShine = new Skill("Moonlight Shine", 20, 30, 0, 2);
+        shadowBlast = new Skill("Shadow Blast", 40, 50, 0, 5);
+    }
+
+    @Override
+    public void useSkill(int skillNumber, GameCharacter target) {
+
+        switch(skillNumber) {
+
+            case 1:
+                if(moonStrike.isSkillAvailable()) {
+
+                    target.takeDamage(moonStrike.getSkillDamage());
+
+                    regenMana(moonStrike.getSkillManaRegen());
+
+                    moonStrike.triggerSkillCooldown();
+                }
+            break;
+
+            case 2:
+                if(moonlightShine.isSkillAvailable() && getCharacterCurrentMana() >= moonlightShine.getSkillManaCost()) {
+
+                    target.takeDamage(moonlightShine.getSkillDamage());
+
+                    useMana(moonlightShine.getSkillManaCost());
+
+                    moonlightShine.triggerSkillCooldown();
+                }
+            break;
+
+            case 3:
+                if(shadowBlast.isSkillAvailable() && getCharacterCurrentMana() >= shadowBlast.getSkillManaCost()) {
+
+                    target.takeDamage(shadowBlast.getSkillDamage());
+
+                    useMana(shadowBlast.getSkillManaCost());
+
+                    shadowBlast.triggerSkillCooldown();
+                }
+            break;
+        }
+    }
+
+    public Skill getMoonStrike() {
+        return moonStrike;
+    }
+
+    public Skill getMoonlightShine() {
+        return moonlightShine;
+    }
+
+    public Skill getShadowBlast() {
+        return shadowBlast;
+    }
+}
---
+++ Characters/Zakkarr.java
@@ -0,0 +1,110 @@
+package Characters;
+
+import Foundation.GameCharacter;
+import Foundation.Skill;
+
+public class Zakkarr extends GameCharacter {
+
+    private Skill guardiansBlade;
+    private Skill shieldOfValor;
+    private Skill deathsReturn;
+
+    private int bladeStacks = 0;
+    private boolean hasRevived = false;
+
+    // Passive: Guardian Warrior
+    private boolean guardianBoost = false;
+
+    public Zakkarr() {
+        super("Zakkarr", "Spirit", "Warrior", 250, 20, 100);
+
+        guardiansBlade = new Skill("Guardian's Blade", 5, 0, 5, 0);
+        shieldOfValor = new Skill("Shield of Valor", 20, 0, 0, 2);
+        deathsReturn = new Skill("Death's Return", 0, 50, 0, 999);
+    }
+
+    // Passive trigger when taking damage
+    @Override
+    public void takeDamage(int amount) {
+        super.takeDamage(amount);
+
+        if(isCharacterAlive()) {
+            guardianBoost = true;
+        }
+    }
+
+    @Override
+    public void useSkill(int skillNumber, GameCharacter target) {
+
+        switch(skillNumber) {
+
+            case 1:
+                if(guardiansBlade.isSkillAvailable()) {
+
+                    int damage = guardiansBlade.getSkillDamage() + (bladeStacks * 5);
+
+                    // Passive bonus
+                    if(guardianBoost) {
+                        damage += damage * 10 / 100;
+                        guardianBoost = false;
+                    }
+
+                    target.takeDamage(damage);
+
+                    regenMana(guardiansBlade.getSkillManaRegen());
+
+                    if(bladeStacks < 2) {
+                        bladeStacks++;
+                    }
+
+                    guardiansBlade.triggerSkillCooldown();
+                }
+            break;
+
+            case 2:
+                if(shieldOfValor.isSkillAvailable()) {
+
+                    int damage = shieldOfValor.getSkillDamage();
+
+                    if(guardianBoost) {
+                        damage += damage * 10 / 100;
+                        guardianBoost = false;
+                    }
+
+                    target.takeDamage(damage);
+
+                    heal(20);
+
+                    shieldOfValor.triggerSkillCooldown();
+                }
+            break;
+
+            case 3:
+                if(!hasRevived && deathsReturn.isSkillAvailable() && getCharacterCurrentMana() >= deathsReturn.getSkillManaCost()) {
+
+                    useMana(deathsReturn.getSkillManaCost());
+
+                    heal(150);
+
+                    regenMana(100);
+
+                    hasRevived = true;
+
+                    deathsReturn.triggerSkillCooldown();
+                }
+            break;
+        }
+    }
+
+    public Skill getGuardiansBlade() {
+        return guardiansBlade;
+    }
+
+    public Skill getShieldOfValor() {
+        return shieldOfValor;
+    }
+
+    public Skill getDeathsReturn() {
+        return deathsReturn;
+    }
+}
